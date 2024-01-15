2007f4f875756b805f85a94bc8dbea6b07df53f8
Sam <hbisedm666@163.com>
2024-01-15 10:55:08 +0800

    fix: performWorkOfUnit return

---
+++ v5/vite-runner/App.jsx
@@ -0,0 +1,27 @@
+
+import React from './core/React.js'
+
+// const App =  React.createElement('div', {id:'app'}, 'app')
+
+const App = <div>
+	<div>
+		<div>1</div>
+		<div>
+		<div>2</div>
+		<div>3</div>
+		</div>
+	</div>
+	<div>32</div>
+</div>
+
+// function a(){
+// 	return <div id="id1">
+// 		<span>1</span>
+// 		<span>2</span>
+// 	</div>
+// }
+// console.log(a)
+
+console.log(App)
+
+export default App
---
+++ v5/vite-runner/core/React.js
@@ -0,0 +1,145 @@
+function createTextNode(text) {
+	return {
+		type: 'TEXT_ELEMENT',
+		props: {
+			nodeValue: text,
+			children:[]
+		},
+	}
+}
+
+
+function createElement(type, props, ...children) {
+	return {
+		type,
+		props: {
+			...props,
+			children: children.map(child => {
+				return typeof child === 'string'? createTextNode(child) : child
+			}),
+		},
+	}
+}
+
+function render(el, container) {
+	nextWorkOfUnit = {
+		dom: container,
+		props: {
+			children: [el]
+		}
+	} 
+
+	root = nextWorkOfUnit
+}
+
+let root = null
+let nextWorkOfUnit = null 
+
+function workLoop(deadline) {
+
+	let shouldYield = false
+	if(!shouldYield && nextWorkOfUnit) {
+		nextWorkOfUnit = performWorkOfUnit(nextWorkOfUnit)
+		shouldYield = deadline.timeRemaining() < 1
+	}
+
+	if(!nextWorkOfUnit) {
+		commitRoot() 
+	}
+
+	requestIdleCallback(workLoop)
+}
+
+function commitRoot() {
+	commitWork(root.child)
+	root = null
+}
+
+function commitWork(fiber) {
+	if(!fiber) return
+	fiber.parent.dom.append(fiber.dom)
+	commitWork(fiber.child)
+	commitWork(fiber.sibling)
+}
+
+function createDom(type) {
+		return  type === 'TEXT_ELEMENT' 
+				? document.createTextNode('') 
+				: document.createElement(type)
+}
+
+function updateProps(dom, props) {
+	Object.keys(props).forEach(key => {
+		if(key !== 'children') {
+		dom[key] = props[key]
+		}
+	})
+}
+
+/**
+ * 1. child
+ * 2. sibling
+ * 3. uncle
+ * @param {*} fiber 
+ */
+function initChildren(fiber) {
+
+	const children  = fiber.props.children
+	let prevChild = null
+	children.forEach((child, index) => {
+		const newFiber = {
+			type: child.type,
+			props: child.props,
+			child: null,
+			parent: fiber,
+			sibling: null,
+			dom: null
+		}
+
+		if(index === 0) {
+			fiber.child = newFiber
+		}else {
+			prevChild.sibling = newFiber
+		}
+		prevChild = newFiber
+	})
+}
+
+
+function  performWorkOfUnit(fiber) {
+	// 1. 创建dom
+	if(!fiber.dom) {
+		const dom = (fiber.dom =  createDom(fiber.type))
+
+	// fiber.parent.dom.append(dom)
+			
+	// 2. 处理props
+	updateProps(dom, fiber.props)
+	}
+
+	// 3. dom树转换为链表 设置好指针
+	initChildren(fiber)
+	// 4. 返回下一个要执行的任务
+	if(fiber.child) {
+		return fiber.child
+	}
+
+	if(fiber.sibling) {
+		return fiber.sibling
+	}
+
+	let nextFiber = fiber
+	while(nextFiber) {
+		if(nextFiber.sibling) return nextFiber.sibling
+		nextFiber = nextFiber.parent
+	}
+}
+
+requestIdleCallback(workLoop)
+
+
+const React = {
+	createElement,
+	render
+}
+export default React
