852fddd445982444a7852af8c24637439f9ebb82
Sam <hbisedm666@163.com>
2024-01-19 10:25:41 +0800

    feat: 实现useState & 批量执行action

---
+++ v13/vite-runner/App.jsx
@@ -0,0 +1,30 @@
+import React from './core/React.js'
+
+// const App =  React.createElement('div', {id:'app'}, 'app')
+function Foo() {
+	const [count, setCount] = React.useState(1)
+	const [bar, setBar] = React.useState('bar')
+
+	function handleClick() {
+		setCount((c) => c+ 1)
+		setBar((b) => b+'bar')
+	}
+
+	return (
+		<div>
+			<h1>foo</h1>
+			{count}
+			<div>{bar}</div>
+			<button onClick={handleClick}>click</button>
+		</div>
+	)
+}
+
+function App() {
+	return <div>
+		hi
+		<Foo></Foo>
+	</div>
+}
+
+export default App
